29c99fb0a6b2f11c7c43cfcb46a575c29cd18cdd
Art Ramirez <artramirez08@gmail.com>
2023-03-02 13:45:09 -0800

    initial commit

---
+++ views/error.jsx
@@ -0,0 +1,12 @@
+const React = require("react");
+
+function error() {
+  return (
+    <main>
+      <h1>PAGE NOT FOUND</h1>
+      <p>Sorry :(</p>
+    </main>
+  );
+}
+
+module.exports = error;
---
+++ views/home.jsx
@@ -0,0 +1,11 @@
+const React = require("react");
+
+function home() {
+  return (
+    <main>
+      <h1>Home Page!</h1>
+    </main>
+  );
+}
+
+module.exports = home;
